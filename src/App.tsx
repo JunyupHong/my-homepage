d087e9793f27cad9bbf0959d603c82b2ed3d4631
JunyupHong <junyup.hong@gmail.com>
2020-12-29 03:03:59 +0900

    [Feat] light & shape 구현
    	mouse 커서에 따라 point light이동
    	5개 shape (cylinder, cone, torus, torusknot, octahedron) 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,126 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.scss';
 import * as THREE from 'three';
-import { DragControls } from 'three/examples/jsm/controls/DragControls';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-
+import { DoubleSide } from 'three';
 
 function App() {
-    
+    enum Shape {
+        CYLINDER = 'cylinder',
+        CONE = 'cone',
+        TORUS = 'torus',
+        TORUSKNOT = 'torusknot',
+        OCTAHEDRON = 'octahedron'
+    };
+
     const canvas = useRef<HTMLDivElement>(null);
     const scene: THREE.Scene = new THREE.Scene();
-    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, 0, 0.1, 1000);
+    const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, 0, 200, 600);
+    const light: THREE.PointLight = new THREE.PointLight(0xffffff, 2, window.innerWidth);
+    // const backLight: THREE.PointLight = new THREE.PointLight(0xffffff, 0.2, window.innerWidth);
     const renderer: THREE.Renderer = new THREE.WebGLRenderer({ antialias: true });
-    const heartShape = new THREE.Shape();
-    const x = 0, y = 0;
+
+    const mousePosition: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    const meshes: Array<THREE.Mesh> = [];
+
     let tick = 0;
-    const update = () => {
-        tick++;
-        requestAnimationFrame(update);
-        heartShape.moveTo( 5, 5 );
-        if (tick % 5 === 0) camera.position.z = 3*Math.sin(tick) + 100;
-        // camera.position.y += Math.sin(tick * 0.1) / 3;
-
-        renderer.render(scene, camera);
-    };
-    
     useEffect(() => {
-        if (!canvas || !canvas.current) return;
-        const width = canvas.current.clientWidth;
-        const height = canvas.current.clientHeight;
-        
-        renderer.setSize(width, height);
-        canvas.current.appendChild(renderer.domElement);
-        
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
-        camera.position.z = 100;
+        initCanvas();
 
-        
-        heartShape.moveTo( x + 5, y - 5 );
-        heartShape.bezierCurveTo( x + 5, y - 5, x + 4, y, x, y );
-        heartShape.bezierCurveTo( x - 6, y, x - 6, y - 7,x - 6, y - 7 );
-        heartShape.bezierCurveTo( x - 6, y - 11, x - 3, y - 15.4, x + 5, y - 19 );
-        heartShape.bezierCurveTo( x + 12, y - 15.4, x + 16, y - 11, x + 16, y - 7 );
-        heartShape.bezierCurveTo( x + 16, y - 7, x + 16, y, x + 10, y );
-        heartShape.bezierCurveTo( x + 7, y, x + 5, y - 5, x + 5, y - 5 );
-        const geometry = new THREE.ShapeGeometry( heartShape );
-        const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-        const mesh = new THREE.Mesh( geometry, material ) ;
-        scene.add( mesh );
+        createShape(Shape.CYLINDER);
+        createShape(Shape.TORUSKNOT);
+        createShape(Shape.TORUS);
+        createShape(Shape.OCTAHEDRON);
+        createShape(Shape.CONE);
+        meshes[0].position.x = -200;
+        meshes[1].position.x = -100;
+        meshes[2].position.x = 0;
+        meshes[3].position.x = 100;
+        meshes[4].position.x = 200;
 
-        const controls = new DragControls( [mesh], camera, renderer.domElement );
+        scene.add(...meshes);
 
-        // add event listener to highlight dragged objects
+        renderer.domElement.addEventListener('mousemove', mouseMove);
 
-        controls.addEventListener( 'dragstart', event => {
-            console.log(event)
-            console.log(event.object.material)
-            event.object.material.color.set(0xaa0000);
-        });
+        updateCanvas();
 
-        controls.addEventListener( 'dragend', function ( event ) {
-            event.object.material.color.set( 0xff0000 );
-        });
-
-        
-
-        // const geometry = new THREE.BufferGeometry();
-        // const vertices = new Float32Array([
-        //     -1.0,
-        //     -1.0,
-        //     1.0,
-        //     1.0,
-        //     -1.0,
-        //     1.0,
-        //     1.0,
-        //     1.0,
-        //     1.0,
-
-        //     1.0,
-        //     1.0,
-        //     1.0,
-        //     -1.0,
-        //     1.0,
-        //     1.0,
-        //     -1.0,
-        //     -1.0,
-        //     1.0,
-        // ]);
-
-        // geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-        // const material = new THREE.MeshBasicMaterial({ color: 0xcccccc });
-        // const mesh = new THREE.Mesh(geometry, material);
-
-        // scene.add(mesh);
-        // console.log(scene);
-
-        update();
-
-
-
+        return () => {
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            if (canvas && canvas.current) canvas.current.removeChild(renderer.domElement);
+            renderer.domElement.removeEventListener('mousemove', mouseMove);
+        };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    const updateCanvas = () => {
+        tick++;
+        meshes.forEach((mesh, i) => {
+            mesh.rotation.x = tick * (i+1) / 300;
+            mesh.rotation.y = tick * (i+1) / 100;
+        });
+        light.position.set(mousePosition.x, mousePosition.y, mousePosition.z);
+        renderer.render(scene, camera);
+        requestAnimationFrame(updateCanvas);
+    };
+
+    const initCanvas = () => {
+        if (!canvas || !canvas.current) return;
+        
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.domElement.setAttribute('style',`
+            position: absolute;
+            z-index: 1;
+            opacity: 0.9;
+        `);
+
+        canvas.current.appendChild(renderer.domElement);
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        camera.position.z = 300;
+
+
+        light.position.set(mousePosition.x, mousePosition.y, mousePosition.z);
+        light.castShadow = true;
+        scene.add(light);
+
+        // backLight.position.set(window.innerWidth / 2, window.innerHeight / 2, 0);
+        // scene.add(backLight);
+    }
+
+    const mouseMove = (evt: MouseEvent) => {
+        console.log(evt);
+        mousePosition.set(evt.offsetX - window.innerWidth / 2, window.innerHeight / 2 - evt.offsetY, 0);
+    }
+
+    const createShape = (shape: Shape) => {
+        let geometry!: THREE.Geometry;
+        const material = new THREE.MeshStandardMaterial( { color: 0x444444, side: DoubleSide } );
+        switch(shape) {
+            case 'cylinder':
+                geometry = new THREE.CylinderGeometry(5, 5, 20, 32);
+                break;
+            case 'cone':
+                geometry = new THREE.ConeGeometry(5, 12, 20);
+                break;
+            case 'torus':
+                geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+                break;
+            case 'torusknot':
+                geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+                break;
+            case 'octahedron':
+                geometry = new THREE.OctahedronGeometry(10, 0);
+                break;
+        }
+
+        meshes.push(new THREE.Mesh( geometry, material ));
+    }
 
     return (
         <div className="app" ref={canvas}>
-            <header className="app-header"></header>
-            <section className="app-section"></section>
-            <footer className="app-footer"></footer>
+            <header className="app-header">header</header>
+            <section className="app-section">section</section>
+            <footer className="app-footer">footer</footer>
         </div>
     );
 }
