3480cf6a65c11908b4f97e13cb8f0342dbcd026d
JunyupHong <junyup.hong@gmail.com>
2020-12-31 23:26:17 +0900

    [Feat] MeshObject 구조 변경 및 background 구현
    	MeshObject 구조 변경
    	app의 background 1차 완성

--- src/App.tsx
+++ src/App.tsx
@@ -8,47 +8,51 @@ function App() {
         CONE = 'cone',
         TORUS = 'torus',
         TORUSKNOT = 'torusknot',
-        OCTAHEDRON = 'octahedron'
+        OCTAHEDRON = 'octahedron',
+        ICOSAHEDRON = 'icosahedron'
+    };
+    type MeshObject = {
+        mesh: THREE.Mesh,
+        positionVector: THREE.Vector3,
+        rotationVector: THREE.Vector3,
+        lifeTime: number,
+        init: () => MeshObject
     };
 
     const canvas = useRef<HTMLDivElement>(null);
     const scene: THREE.Scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x010101);
     const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const light: THREE.PointLight = new THREE.PointLight(0xffffff, 2, 100, 2, );
-    // const light: THREE.SpotLight = new THREE.SpotLight(0xffffff, 1);
+    const light: THREE.PointLight = new THREE.PointLight(0xffffff, 2, 100, 2);
     const renderer: THREE.Renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 
-    let mousePosition: THREE.Vector3 = new THREE.Vector3(0, 0, window.innerWidth * 2);
+    const mousePosition: THREE.Vector3 = new THREE.Vector3(0, 0, 50);
 
-    const objects: Array<{
-        mesh: THREE.Mesh,
-        speed: number
-        rotationSpeed: number,
-    }> = [];
+    const meshObjects: Array<MeshObject> = [];
+    const objectCount: number = 50;
 
-    let tick = 0;
+    let tick = 1;
     useEffect(() => {
         initCanvas();
 
-        objects.push({
-            mesh: createShapeMesh(Shape.CYLINDER),
-            speed: Math.random(),
-            rotationSpeed: Math.random(),
-        });
-        
-        objects[0].mesh.position.x = 0;
-        objects[0].mesh.position.z = -10;
+        for (let i = 0; i < objectCount; i++) {
+            meshObjects.push(createShapeMeshObject(Shape.CONE).init());
+            meshObjects.push(createShapeMeshObject(Shape.CYLINDER).init());
+            meshObjects.push(createShapeMeshObject(Shape.OCTAHEDRON).init());
+            meshObjects.push(createShapeMeshObject(Shape.TORUS).init());
+            // meshObjects.push(createShapeMeshObject(Shape.TORUSKNOT).init());
+            meshObjects.push(createShapeMeshObject(Shape.ICOSAHEDRON).init());
+        }
 
-        scene.add(...objects.map(object => object.mesh));
-
+        scene.add(...meshObjects.map(object => object.mesh));
         window.addEventListener('resize', onResizeWindow);
         renderer.domElement.addEventListener('mousemove', onMouseMove);
+        
         updateCanvas();
 
         return () => {
             // eslint-disable-next-line react-hooks/exhaustive-deps
             if (canvas && canvas.current) canvas.current.removeChild(renderer.domElement);
+            while (meshObjects.length) meshObjects.pop();
             window.removeEventListener('resize', onResizeWindow);
             renderer.domElement.removeEventListener('mousemove', onMouseMove);
         };
@@ -57,18 +61,27 @@ function App() {
 
     const updateCanvas = () => {
         tick++;
-        // meshes.forEach((mesh, i) => {
-        //     mesh.rotation.x = tick * (i+1) / 300;
-        //     mesh.rotation.y = tick * (i+1) / 100;
-        // });
-        light.position.set(mousePosition.x, mousePosition.y, 0);
+        meshObjects.forEach(object => {
+            if (tick % object.lifeTime === 0) object.init();
+            if (object.mesh.position.y > window.innerHeight / 3) object.init();
+            object.mesh.position.x += object.positionVector.x;
+            object.mesh.position.y += object.positionVector.y;
+            object.mesh.position.z += object.positionVector.z;
+
+            object.mesh.rotation.x += object.rotationVector.x * Math.PI / 180;
+            object.mesh.rotation.y += object.rotationVector.y;
+            object.mesh.rotation.z += object.rotationVector.z;
+        });
+
+        light.position.set(mousePosition.x, mousePosition.y, mousePosition.z);
         renderer.render(scene, camera);
         requestAnimationFrame(updateCanvas);
     };
 
     const initCanvas = () => {
-        if (!canvas || !canvas.current) return;
-        
+        scene.background = new THREE.Color(0x010101);
+
+        if (!canvas || !canvas.current) return;        
         renderer.setSize(window.innerWidth, window.innerHeight);
         renderer.domElement.setAttribute('style',`
             position: absolute;
@@ -81,8 +94,6 @@ function App() {
         camera.position.z = 300;
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-        
-
 
         light.position.set(mousePosition.x, mousePosition.y, mousePosition.z);
         light.castShadow = true;
@@ -117,8 +128,28 @@ function App() {
             case 'octahedron':
                 geometry = new THREE.OctahedronGeometry(10, 0);
                 break;
+            case 'icosahedron':
+                geometry = new THREE.IcosahedronGeometry(10);
         }
-        return new THREE.Mesh( geometry, material )
+        return new THREE.Mesh(geometry, material);
+    }
+
+    const createShapeMeshObject = (shape: Shape): MeshObject => {
+        const object: MeshObject = {
+            mesh: createShapeMesh(shape),
+            positionVector: new THREE.Vector3(),
+            rotationVector: new THREE.Vector3(),
+            lifeTime: 0,
+            init: (): MeshObject => {
+                object.mesh.position.set(-(window.innerWidth / 2) * 75 / 180 + (Math.random() - 0.5) * 100, (window.innerHeight / 2) * 75 / 180 + (Math.random() - 0.5) * 100 - 100, 0);
+                object.mesh.rotateZ(Math.PI / 2 + Math.sin(Math.random() - 0.5));
+                object.positionVector.set((Math.random() + 0.2) * 0.6, (Math.random() - 0.7) * 0.6, Math.random() / 2 - 0.25);
+                object.rotationVector.set((Math.random() - 0.5) * 0.05, (Math.random() - 0.5) * 0.05, (Math.random() - 0.5) * 0.03);
+                object.lifeTime = Math.floor(700 + Math.random() * 600);
+                return object;
+            }
+        };
+        return object.init();
     }
 
     return (
